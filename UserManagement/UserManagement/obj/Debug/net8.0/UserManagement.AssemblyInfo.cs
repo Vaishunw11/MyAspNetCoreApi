5f96534c5a9a15772f44434bbcfc706d16fba315
Vaishnavi Walke <walkevaishu37@gmail.com>
2025-08-29 13:13:46 +0530

    Update changes

--- UserManagement/UserManagement/obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
+++ UserManagement/UserManagement/obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71eea419b34deb618e7a7bedfec2659265364762")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aa041b7165cc4d5d36dcd0393300a19faea6aac")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
